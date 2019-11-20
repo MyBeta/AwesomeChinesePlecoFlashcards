a9bc181f6c850fb000a29e86c58a593a2dc3833e
Michael Bögli <michael.boegli@gmail.com>
2019-11-20 17:09:30 +0800

    added comments and new quizlet export formats

--- quizletToPleco.py
+++ quizletToPleco.py
@@ -9,20 +9,29 @@ converts flashcards from quizlet to pleco format
 
 1) on quizlet.com click on export, select "separate by tab", and "new line" then copy text.
 2) run this script
-3) everything is saved under output
+3) everything is saved in the output variable
 """
 
+#%% Imports
 import pyperclip
 import re
+
+#%% read from clipboard and split by new line
 data = pyperclip.paste() 
 a = data.split("\n")
 
+#%% extract content for each line
 # regexp replace each line 
+# format: characters<tab>english<space>(pinyin)
 #exp = r'([^\t]*)\t([^\(（]*)[\(（]([^\)）]*)[\)）]'
 exp = r'([^\t]*)\t(.*)[\(（]([^\)）]*)[\)）]'
 sub = r'\1\t\3\t\2'
+# format: characters<space>(pinyin)<tab>english
+# exp = r'([^\s]*)\s[\(（]([^\)）]*)[\)）]\t(.*)'
+# sub = r'\1\t\2\t\3'
 replace_st = lambda x : re.sub(exp, sub, x)
 
 b = list( map(replace_st, a))
 
+#%% save output
 output = "\n".join(b)
